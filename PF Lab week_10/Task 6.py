493864efa7102507db29381ffff763a0c0e54aaa
mariaabbas066-rgb <mariaabbas066@gmail.com>
2026-05-25 19:30:18 +0500

    Add files via upload

---
+++ PF Lab week_10/Task 6.py
@@ -0,0 +1,3 @@
+print("Name : Maria")
+print("Age : 19 years old")
+print("Roll Number : 2026(S)-CYS-61")
---
+++ PF Lab week_10/Task 8.py
@@ -0,0 +1,6 @@
+a=[1,"Maria","Javaria"]
+print(a)
+print(a[1])
+print(a[0])
+a.append("Hafsa")
+print(a)
